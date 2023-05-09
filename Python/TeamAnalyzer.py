a62c1b054dbedad5a7feb617b2955586890ab1fc
megbalf <megbalf@uw.edu>
2023-05-09 00:06:04 -0700

    Finally finished the HW

--- Python/TeamAnalyzer.py
+++ Python/TeamAnalyzer.py
@@ -6,6 +6,9 @@ types = ["bug","dark","dragon","electric","fairy","fight",
     "fire","flying","ghost","grass","ground","ice","normal",
     "poison","psychic","rock","steel","water"]
 
+conn = sqlite3.connect('../pokemon.sqlite')
+cursor = conn.cursor()
+
 # Take six parameters on the command-line
 if len(sys.argv) < 6:
     print("You must give me six Pokemon to analyze!")
@@ -16,12 +19,41 @@ for i, arg in enumerate(sys.argv):
     if i == 0:
         continue
 
-    # Analyze the pokemon whose pokedex_number is in "arg"
+    name = cursor.execute("SELECT name FROM pokemon WHERE pokedex_number = " + arg).fetchall()
+  
 
-    # You will need to write the SQL, extract the results, and compare
-    # Remember to look at those "against_NNN" column values; greater than 1
-    # means the Pokemon is strong against that type, and less than 1 means
-    # the Pokemon is weak against that type
+    pokename = name[0][0]
+    print(pokename)
+
+    name2 = cursor.execute("SELECT * FROM pokemon_types_view WHERE name = '" + pokename + "'").fetchone()
+
+    pokename1 = name2[1]
+    pokename2 = name2[2]
+    # print(pokename1, pokename2)
+   
+    against = cursor.execute("SELECT * FROM pokemon_types_battle_view WHERE type1name = '" + pokename1 + "' and type2name = '" + pokename2 + "'").fetchone()
+    # print(against)
+
+    #creates clean tuple without the first two columns 
+    clean_pokemon_types = against[2:]
+    # print(clean_pokemon_types)
+
+    stronger_than = []
+    weaker_than = []
+
+    count = 0
+    
+    for column in clean_pokemon_types:
+        if column >= 1:
+            stronger_than.append(types[count])
+            
+        else: 
+            weaker_than.append(types[count]) 
+        count += 1
+        # print(count)
+    print(pokename + "(" + pokename1 + ", " + pokename2 +  ") is strong againsgt:" + str(stronger_than) + "but weak against:" + str(weaker_than))
+  
+
 
 answer = input("Would you like to save this team? (Y)es or (N)o: ")
 if answer.upper() == "Y" or answer.upper() == "YES":
@@ -31,4 +63,3 @@ if answer.upper() == "Y" or answer.upper() == "YES":
     print("Saving " + teamName + " ...")
 else:
     print("Bye for now!")
-
